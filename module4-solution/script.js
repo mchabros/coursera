9d1abea2e24b7baeb2afab120e1b01c8cfe6fb55
mchabros <maja.chabros@gmail.com>
2018-12-09 15:31:48 +0100

    module5

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -54,7 +54,7 @@ WARNING!!! WARNING!!!
     // Look up these methods on Mozilla Developer Network web site if needed.
      var firstLetter = names[i].charAt(0);
 
-     if ( firstLetter.toLowerCase() === "j" ) {
+    if ( firstLetter.toLowerCase() === "j" ) {
       byeSpeaker.speak(names[i]);
      } else {
       helloSpeaker.speak(names[i]);
